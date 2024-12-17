2e4f331017ffa115e6511495cfa608876c4c664c
Mochi <mochitreets0304@gmail.com>
2024-12-17 22:27:56 +0800

    axios套件

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // process.env.NODE_ENV 是抓取目前環境狀態(開發/上線)的意思
+  // production是上線(產品)環境
+  base:process.env.NODE_ENV==='production' ? '/react-gh-sample/' : '/',
   plugins: [react()],
 })
